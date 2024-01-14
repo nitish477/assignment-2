e88a4adddcb0e61e721bfa3eea0822519bcbb323
nitish477 <krnitish540@gmail.com>
2024-01-14 14:06:30 +0530

    Hosting

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 import asyncHandler from "express-async-handler";
 import routes from './routes/routes.js';
+import path from 'path';
 
 const app = express()
 app.use(express.json())
-
+const __dirname = path.resolve();
 const connetDB= asyncHandler(async()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
@@ -22,6 +23,15 @@ const connetDB= asyncHandler(async()=>{
 app.use('/api/v1',routes)
 
 const port = process.env.PORT || 5000
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  });
+}
+
 app.listen(port,()=>{
     console.log(`Server started on ${port}`)
     connetDB()
